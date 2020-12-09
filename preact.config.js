5033100f1bc352e63985c024c2cb733ae480bfb3
Adrien Pyke <adrien.pyke@shopify.com>
2020-12-08 19:21:17 -0500

    gh-pages config

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,13 @@
-export default (config, env) => {
+export default (config, env, helpers, options) => {
   config.resolve.modules.push(env.src + '/app')
+
+  const publicPath = process.env.GITHUB_PAGES
+    ? `/${process.env.GITHUB_PAGES}/`
+    : '/'
+  const ghEnv =
+    process.env.GITHUB_PAGES && JSON.stringify(`${process.env.GITHUB_PAGES}`)
+
+  config.output.publicPath = publicPath
+  const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0]
+  Object.assign(plugin.definitions, { 'process.env.GITHUB_PAGES': ghEnv })
 }
