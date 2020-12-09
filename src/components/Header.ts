8a094b32ce6ab39754da13a8fe66090cfe0bba78
Adrien Pyke <adrien.pyke@shopify.com>
2020-12-09 13:26:00 -0500

    header box shadow

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,11 +1,11 @@
-import { m } from '/vdom'
+import { m, z } from '/vdom'
 import { Dropdown } from '/components'
 import { useStore, setDay } from '/store'
 
 export const Header = () => {
   const day = useStore(({ day }) => day);
   return m(
-    'header.navbar.p-2.bg-primary.text-light',
+    'header.navbar.p-2.bg-primary.text-light' + z`box-shadow 0 2px 4px rgba(0, 0, 0, 0.5);`,
     m('section.navbar-section', [
       m(
         'a.navbar-brand.text-bold.mx-2.text-light',
