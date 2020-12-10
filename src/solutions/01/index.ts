981a95437840dc2244d90907733e6484e8c9b2e4
Adrien Pyke <adrien.pyke@shopify.com>
2020-12-09 19:32:19 -0500

    day 1

--- src/solutions/01/index.ts
+++ src/solutions/01/index.ts
@@ -1,5 +1,29 @@
 import { m } from '/vdom'
+import input from './input'
+import { nestedLoop, sum } from '/utilities'
 
-export const Part1 = () => m('div', 'Part 1')
+const parseInput = () => input.split('\n').map(Number)
 
-export const Part2 = () => m('div', 'Part 2')
+const findNumbersThatSum = (
+  nums: number[],
+  expectedSum: number,
+  howMany = 2
+) => {
+  for (const indexes of nestedLoop(howMany, 0, nums.length - 1)) {
+    const set = indexes.map((i) => nums[i])
+    if (set.reduce(sum) === expectedSum) return set
+  }
+}
+
+export const Part1 = () => {
+  const [n1, n2] = findNumbersThatSum(parseInput(), 2020)!
+  return m('div', `Numbers ${n1} and ${n2} sum to 2020. Result is ${n1 * n2}`)
+}
+
+export const Part2 = () => {
+  const [n1, n2, n3] = findNumbersThatSum(parseInput(), 2020, 3)!
+  return m(
+    'div',
+    `Numbers ${n1}, ${n2}, and ${n3} sum to 2020. Result is ${n1 * n2 * n3}`
+  )
+}
--- src/utilities.ts
+++ src/utilities.ts
@@ -5,3 +5,25 @@ export const range = (start: number, end: number) => {
   }
   return arr
 }
+
+export const sum = (a: number, b: number) => a + b
+
+export const nestedLoop = function* (
+  n: number,
+  start: number,
+  end: number
+): IterableIterator<number[]> {
+  const arr = [...Array(n)].map(() => start)
+  let i = 0
+  while (true) {
+    yield arr.slice()
+    arr[0]++
+    while (arr[i] === end + 1) {
+      arr[i] = start
+      i++
+      if (i === n) return
+      arr[i]++
+      if (arr[i] !== end + 1) i = 0
+    }
+  }
+}
