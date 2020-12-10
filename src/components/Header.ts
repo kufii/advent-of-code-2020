caf0d47beafd43d431167859e47e6ff81347de33
Adrien Pyke <adrien.pyke@shopify.com>
2020-12-09 21:10:14 -0500

    add github link

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,31 +1,44 @@
-import { m, z, href } from '/vdom'
+import { m, z, href, newTab } from '/vdom'
 import { Dropdown } from '/components'
 import { useStore, setDay, setPart } from '/store'
 import { range } from '/utilities'
+import { githubIcon } from './images'
+
+console.log(githubIcon)
 
 export const Header = () => {
   const day = useStore(({ day }) => day)
   return m(
     'header.navbar.p-2.bg-primary.text-light' +
       z`box-shadow 0 2px 4px rgba(0, 0, 0, 0.5);`,
-    m('section.navbar-section', [
+    [
+      m('section.navbar-section', [
+        m(
+          'a.navbar-brand.text-bold.mx-2.text-light',
+          { href: href('/') },
+          'Advent of Code 2020'
+        ),
+        m(Dropdown, {
+          selected: day,
+          onSelect: (day) => {
+            setPart(null)
+            setDay(day)
+          },
+          items: range(1, 25).map((n) => ({
+            key: n.toString(),
+            text: `Day ${n}`
+          })),
+          textColor: 'light'
+        })
+      ]),
       m(
-        'a.navbar-brand.text-bold.mx-2.text-light',
-        { href: href('/') },
-        'Advent of Code 2020'
-      ),
-      m(Dropdown, {
-        selected: day,
-        onSelect: (day) => {
-          setPart(null)
-          setDay(day)
-        },
-        items: range(1, 25).map((n) => ({
-          key: n.toString(),
-          text: `Day ${n}`
-        })),
-        textColor: 'light'
-      })
-    ])
+        'section.navbar-section',
+        m(
+          'a.btn.btn-link',
+          { href: 'https://github.com/kufii/advent-of-code-2020', ...newTab },
+          m('img' + z`height 100%`, { src: githubIcon, alt: 'GitHub' })
+        )
+      )
+    ]
   )
 }
---
+++ src/components/images/index.ts
@@ -0,0 +1,1 @@
+export { default as githubIcon } from './GitHub-Mark-Light-120px-plus.png'
