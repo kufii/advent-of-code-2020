f64809af4526456447739b3e5a3ca70e13ba084a
Adrien Pyke <adrien.pyke@shopify.com>
2020-12-10 19:35:11 -0500

    day 7

---
+++ src/solutions/07/index.ts
@@ -0,0 +1,63 @@
+import input from './input'
+import { m } from '/vdom'
+
+type Bag = Record<string, number>
+type Bags = Record<string, Bag>
+
+const parseInput = () =>
+  input
+    .split('\n')
+    .map(
+      (line) =>
+        line.match(/^(?<bag>[a-z ]+) bags contain (?<contains>[a-z\d, ]+).$/u)!
+          .groups!
+    )
+    .reduce(
+      (acc, { bag, contains }) => ({
+        ...acc,
+        [bag]:
+          contains === 'no other bags'
+            ? {}
+            : contains
+                .split(', ')
+                .map(
+                  (part) =>
+                    part.match(/^(?<num>\d+) (?<bag>.+) bags?$/u)!.groups!
+                )
+                .reduce(
+                  (acc, { num, bag }) => ({ ...acc, [bag]: Number(num) }),
+                  {} as Bag
+                )
+      }),
+      {} as Bags
+    )
+
+const bagContains = (bags: Bags, bag: string, contains: string): boolean =>
+  Boolean(bags[bag][contains]) ||
+  Object.keys(bags[bag]).some((bag) => bagContains(bags, bag, contains))
+
+const countBags = (bags: Bags, bag: string): number =>
+  Object.entries(bags[bag]).reduce(
+    (sum, [bag, count]) => sum + count + countBags(bags, bag) * count,
+    0
+  )
+
+export const Part1 = () => {
+  const bags = parseInput()
+  const count = Object.keys(bags).filter((bag) =>
+    bagContains(bags, bag, 'shiny gold')
+  ).length
+  return m(
+    'div',
+    m('strong', count),
+    ' bags can eventually contain shiny gold bags.'
+  )
+}
+
+export const Part2 = () =>
+  m(
+    'div',
+    'Shiny gold bags contain ',
+    m('strong', countBags(parseInput(), 'shiny gold')),
+    ' other bags.'
+  )
